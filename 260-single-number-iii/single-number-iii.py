f6c8b99e0aa5193b5f59aae62fb957c10103de6f
Rahim Nabi <rnabi6632@gmail.com>
2026-09-05 22:40:04 +0530

    Time: 6 ms (19.79%) | Memory: 21 MB (42.93%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = []
+        freq_map = {}
+        for num in nums:
+            if num in freq_map:
+                freq_map[num] += 1
+            else:
+                freq_map[num] = 1
+        for num in nums:
+            if freq_map[num] == 1:
+                res.append(num)
+
+        return res
